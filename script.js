897c47d851f644c6fcab94cb7e97e0fd587d9ed9
samukzxp <135071828+samukzxp@users.noreply.github.com>
2024-11-09 12:17:04 -0300

    Atualizando o código JavaScript via API

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
+
 function sensi() {
-    window.location.href = "sensibilidades.html";
+window.location.href = "sensibilidades.html";
 }
 
 function insta() {
-    window.open("https://www.instagram.com/vz_vendas._/", "_blank");
+window.open("https://www.instagram.com/vz_vendas._/", "_blank");
 }
 
 function grupo() {
-    window.location.href = "";
+window.location.href = "https://chat.whatsapp.com/DM0achM0esa7r8ceRhtn0Y";
 }
 
 function headtracking() {
