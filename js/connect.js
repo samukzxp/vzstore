9a9b1b9f63c6a539316b3f47294339d62a467fea
samukzxp <135071828+samukzxp@users.noreply.github.com>
2024-11-10 19:09:34 -0300

    Create connect.js

---
+++ js/connect.js
@@ -0,0 +1,7 @@
+// Defina as variáveis necessárias para acessar o repositório
+const token = "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"; // Insira seu token de acesso pessoal do GitHub
+const repoOwner = "samukzxp"; // Seu nome de usuário no GitHub
+const repoName = "bdvz"; // Nome do repositório onde deseja salvar os dados
+const pathOwner = "dados/owner.json"; // Caminho do arquivo owner.json
+const path = "dados/usuarios.json"; // Caminho do arquivo JSON no repositório
+const fileName = 'script.js'; // Nome do arquivo no repositório
