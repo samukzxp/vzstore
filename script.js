55dfdfb22e417eef600a7297b5c263def6e53bc2
samukzxp <135071828+samukzxp@users.noreply.github.com>
2024-11-11 11:35:10 -0300

    Atualizando o código JavaScript via API

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
 
-function sensi() {
-window.location.href = "sensibilidades.html";
-}
-
-function insta() {
-window.open("https://www.instagram.com/vz_vendas._/", "_blank");
-}
-
-function grupo() {
-window.location.href = "vz";
-}
-
-function headtracking() {
-    window.location.href = "headtracking.html";
-}
+        function grupo() {
+        window.location.href = "vz";
+        }
+    
